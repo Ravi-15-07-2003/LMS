9bfd59ec1dd4ac0be439d511677f830daee92151
Ravi Singh bais <ravisinghbais5@gmail.com>
2025-10-03 10:58:21 +0530

    FireBase authentication and add cloudinary and add pages in frontend and others

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -1,16 +1,16 @@
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginlms-a7ea1.firebaseapp.com",
-  projectId: "loginlms-a7ea1",
-  storageBucket: "loginlms-a7ea1.firebasestorage.app",
-  messagingSenderId: "665916718747",
-  appId: "1:665916718747:web:16dbe0bfe5aeeface0903e"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "lmslogin-35066.firebaseapp.com",
+  projectId: "lmslogin-35066",
+  storageBucket: "lmslogin-35066.firebasestorage.app",
+  messagingSenderId: "495813735629",
+  appId: "1:495813735629:web:282afa63dec0a281e5a584"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
-export {auth,provider}
+export {auth,provider}  
